d82f57c1a9aa6466dc0289ce0441a4acbf8e41a2
Dmitry Ponyatov <dponyatov@samsa.local>
2024-04-25 18:10:42 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@samsa.local>

--- Fsh/utils.fs
+++ Fsh/utils.fs
@@ -1,1 +1,21 @@
 module utils
+
+// /// single coordinate
+// type Coord =
+//     | F of float32
+//     | I of int
+//
+// /// 2D point (on flat surface)
+// type Point2D = Coord * Coord
+//
+// /// 3D point
+// type Point3D = Coord * Coord * Coord
+//
+// /// Point variant
+// type Point =
+//     | Point2D
+//     | Point3D
+
+type Point = float * float * float
+
+type Line = Point * Point
