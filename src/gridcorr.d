b4869ed95a74311d924417714d7e4b746d56797d
Dmitry Ponyatov <dponyatov@samsa.local>
2024-05-06 15:48:19 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@samsa.local>

--- src/gridcorr.d
+++ src/gridcorr.d
@@ -1,13 +1,27 @@
 import std.stdio;
 import std.range;
+import std.file;
 
 void main(string[] args) {
     arg(0, args[0]);
     foreach (argc, argv; args[1 .. $].enumerate(1)) {
         arg(argc, argv);
+        auto nc = File(argv ~ ".corr", "w");
+        nc.writeln(gcode(readText(argv)));
+        nc.close();
     }
 }
 
 void arg(size_t argc, string argv) {
     writefln("argv[%d] = <%s>", argc, argv);
 }
+
+import pegged.grammar;
+
+mixin(grammar("
+gcode:
+    syntax < (perc | comment | any)*
+    perc < '%'
+    comment <~'(' .*? ')'
+    any  < .
+"));
