5b4478fd88ab02899627ab79dd4c303b95c6f931
Dmitry Ponyatov <dponyatov@samsa.local>
2024-05-06 16:36:46 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@samsa.local>

--- src/gridcorr.d
+++ src/gridcorr.d
@@ -2,11 +2,13 @@ import std.stdio;
 import std.range;
 import std.file;
 
+File nc; /// output .nc
+
 void main(string[] args) {
     arg(0, args[0]);
     foreach (argc, argv; args[1 .. $].enumerate(1)) {
         arg(argc, argv);
-        auto nc = File(argv ~ ".corr", "w");
+        nc = File(argv ~ ".corr", "w");
         nc.writeln(gcode(readText(argv)));
         nc.close();
     }
@@ -18,10 +20,33 @@ void arg(size_t argc, string argv) {
 
 import pegged.grammar;
 
+ParseTree a_nl(ParseTree p) @trusted {
+    nc.write("\n");
+    return p;
+}
+
+ParseTree a_g(ParseTree p) @trusted {
+    if (p.matches.length>1)
+        nc.writef("%s\n",p.children);
+    return p;
+}
+
 mixin(grammar("
 gcode:
-    syntax < (perc | comment | any)*
-    perc < '%'
-    comment <~'(' .*? ')'
-    any  < .
+    syntax < ( nl | ws | perc | comment | m | ss | g | x | y | z | f | any )*
+    nl      <~ [\r\n]+ {a_nl}
+    perc    <~ '%'
+    comment <~ '(' (!')' .)* ')'
+    m       <~ 'M' n
+    ss      <~ 'S' n
+    g       <~ ( 'G' n ) {a_g}
+    x       <~ 'X' d
+    y       <~ 'Y' d
+    z       <~ 'Z' d
+    f       <~ 'F' d
+    d       <~ sn '.' n?
+    sn      <~ [-+]? n
+    n       <~ [0-9]+
+    ws      <  [ \t\r\n\f]+
+    any     < .
 "));
